eab28617b7b4b0f9bdc5cc19ef06286632d43c86
Scrush <stuffz597@gmail.com>
2024-06-22 22:49:50 -0700

    Scene Change in Customer Select

--- Fashioning Tomorrow/Assets/Scripts/SceneChangeonCustomer.cs
+++ Fashioning Tomorrow/Assets/Scripts/SceneChangeonCustomer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SceneChangeonCustomer : MonoBehaviour
 {
     void onMouseClick(){
         SceneManager.LoadScene("Dressup");
